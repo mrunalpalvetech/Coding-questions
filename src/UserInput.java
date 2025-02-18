0a9b98ab32541415c1af0ea09ea67c5ef75c10cc
mrunalpalvetech <mrunalpalvetech@gmail.com>
2025-02-18 22:09:11 +0530

    Java interview coding questions

---
+++ src/UserInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class UserInput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter numbers separated by space:");
+        String input = sc.nextLine();
+
+        Stream.of(input.split("\\s+"))
+                .map(Integer::parseInt)  // Lambda shorthand for parsing integers
+                .forEach(num -> System.out.println("Number: " + num));
+
+        System.out.println("Input first number : ");
+        int num1 = Integer.parseInt(sc.nextLine().trim());
+
+        System.out.println("Input second number : ");
+        int num2 = Integer.parseInt(sc.nextLine().trim());
+
+        System.out.println(num1 + num2);
+
+        sc.close();
+    }
+}
